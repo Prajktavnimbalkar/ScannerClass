87f24a1cc9aade06dea4a186c07ac4a29e86f498
Prajktavnimbalkar <147031604+Prajktavnimbalkar@users.noreply.github.com>
2024-01-24 09:28:37 +0530

    Add files via upload

---
+++ Exe2.java
@@ -0,0 +1,17 @@
+package scannerclass;
+import java.util.Scanner;
+public class Exe2 {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		System.out.println("1st no");
+		int n1 = sc.nextInt();
+		System.out.println("2nd no");
+		int n2 = sc.nextInt();
+		System.out.println("3rd no");
+		int n3 = sc.nextInt();
+		int sub = n1-n2-n3;
+		System.out.println("output = "+sub);
+		
+	}
+
+}

f3b9b67b7dfeb83a2e92fdb63ee75101fbe0cc5e
Prajktavnimbalkar <147031604+Prajktavnimbalkar@users.noreply.github.com>
2024-01-25 09:05:49 +0530

    Add files via upload

---
+++ Exe3.java
@@ -0,0 +1,15 @@
+package scannerclass;
+import java.util.Scanner;
+public class Exe3 {
+	public static void main(String[] args) {
+	Scanner sc = new Scanner(System.in);
+	System.out.println("1st no");
+	int n1 = sc.nextInt();
+	System.out.println("2nd no");
+	int n2 = sc.nextInt();
+	int multi = n1 * n2; 
+	System.out.println("ans = " + multi);
+	}	
+	}
+
+
